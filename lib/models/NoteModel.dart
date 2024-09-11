edd5546696814641116d7e042b2c95da8260327e
ashraf <bakr29420@gmail.com>
2024-09-11 23:29:16 +0300

    create NoteModel

---
+++ lib/models/NoteModel.dart
@@ -0,0 +1,9 @@
+class Notemodel {
+  final String title, description, dateTime;
+  final int color;
+  const Notemodel(
+      {required this.dateTime,
+      required this.description,
+      required this.title,
+      required this.color});
+}
