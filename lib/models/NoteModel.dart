309849f9669885ebc052c9c949b4e3ed281d60c7
ashraf <bakr29420@gmail.com>
2024-09-12 00:31:30 +0300

    Generating NodeModel Adapter

--- lib/models/NoteModel.dart
+++ lib/models/NoteModel.dart
@@ -1,7 +1,16 @@
-class Notemodel {
-  final String title, description, dateTime;
+import 'package:hive/hive.dart';
+part 'NoteModel.g.dart';
+@HiveType(typeId: 0)
+class Notemodel extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String description;
+  @HiveField(2)
+  final String dateTime;
+  @HiveField(3)
   final int color;
-  const Notemodel(
+  Notemodel(
       {required this.dateTime,
       required this.description,
       required this.title,
